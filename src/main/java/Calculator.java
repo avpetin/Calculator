766c12da4a5c599b22fc832ba95acc0773badbb6
Alexey Petin <alex_beholder@mail.ru>
2023-03-01 12:40:54 +0300

    Fix1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,7 +8,10 @@ public class Calculator {
     BinaryOperator<Integer> plus = Integer::sum;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) ->  x / y;
+    BinaryOperator<Integer> divide = (x, y) ->  {
+        if(y != 0) return x / y;
+        return Integer.MAX_VALUE;
+    };
 
 
     UnaryOperator<Integer> pow = x -> x * x;
